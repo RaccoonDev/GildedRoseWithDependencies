b5e406b0f08292536ae838c38fd0e11579f13fb9
Dmitry Mikhaylov <dmitry.pyrojoke.mikhaylov@gmail.com>
2017-09-29 12:12:10 +0300

    Modified a bit

---
+++ GildedRose/Check.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace GildedRose
+{
+    class CheckDb
+    {
+        private List<Item> _items;
+        private SqlConnection _conn;
+
+        public CheckDb(List<Item> items, SqlConnection conn)
+        {
+            _items = items;
+            _conn = conn;
+        }
+
+        public int Check()
+        {
+            int n;
+            using (var c = new SqlCommand("select top 1 dates from runs order by dates desc", _conn))
+            {
+                var dbDate = c.ExecuteScalar();
+                if (dbDate == null) n = 1;
+                else n = (DateTime.Now - (DateTime) dbDate).Days;
+            }
+
+            using (var c2 = new SqlCommand("select * from store", _conn))
+            {
+                using (var r = c2.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        _items.Add(new Item
+                        {
+                            Name = r["name"].ToString(),
+                            Quality = (int) r["quality"],
+                            SellIn = (int) r["sellIn"]
+                        });
+                    }
+                }
+            }
+            return n;
+        }
+    }
+}
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -11,31 +11,8 @@ namespace GildedRose
             using (var conn = new SqlConnection(args[0]))
             {
                 conn.Open();
-                int n;
-                using (var c = new SqlCommand("select top 1 dates from runs order by dates desc", conn))
-                {
-                    var dbDate = c.ExecuteScalar();
-                    if (dbDate == null) n = 1;
-                    else n = (DateTime.Now - (DateTime)dbDate).Days;
-                }
-
-                var items = new List<Item>();
-
-                using (var c2 = new SqlCommand("select * from store", conn))
-                {
-                    using (var r = c2.ExecuteReader())
-                    {
-                        while (r.Read())
-                        {
-                            items.Add(new Item
-                            {
-                                Name = r["name"].ToString(),
-                                Quality = (int) r["quality"],
-                                SellIn = (int) r["sellIn"]
-                            });
-                        }
-                    }
-                }
+                List<Item> items = new List<Item>();
+                var n = new CheckDb(items, conn).Check();
 
                 var gsStore = new GildedRose(items);
                 gsStore.Connection = conn;
